fd3bc750bcd9021380b38e06aecf06b228d4d224
Firooz <mirhousainee@gmail.com>
2024-05-29 19:14:55 -0700

    Second commit

---
+++ src/leetCode/NewClass.java
@@ -0,0 +1,12 @@
+package leetCode;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double max = Double.NEGATIVE_INFINITY;
+		
+
+	}
+
+}
